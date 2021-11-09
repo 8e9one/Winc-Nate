26489370c0e37e35ee78d3b1c51b5b789672a29a
8e9one <33422357+8e9one@users.noreply.github.com>
2021-11-09 19:12:47 +0100

    Add files via upload

---
+++ Javascript opdracht1/script.js
@@ -0,0 +1,20 @@
+console.log("Hello Winc Academy");
+let firstName = "Nathaniel";
+console.log(firstName);
+//Uncaught SyntaxError: unexpected token: keyword 'if'
+let numOne = 4;
+let numTwo = 4;
+console.log(numOne + numTwo);
+let stringOne = "4";
+let stringTwo = "4";
+console.log(stringOne + stringTwo);
+console.log(numOne + numTwo);
+console.log(numOne - numTwo);
+console.log(numOne * numTwo);
+console.log(numOne / numTwo);
+console.log(numOne % numTwo);
+let myAge = 33;
+console.log(typeof myAge);
+let myAgeText = "33";
+console.log(typeof myAgeText);
+
