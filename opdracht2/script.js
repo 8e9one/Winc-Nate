b8db69430e509f3012d1434435bbf8a1f39e6f8f
8e9one <33422357+8e9one@users.noreply.github.com>
2021-11-11 20:40:24 +0100

    Add files via upload

---
+++ opdracht2/script.js
@@ -0,0 +1,16 @@
+console.log("Hello Winc Academy students")
+let num1 = 1230941;
+let num2 = 1823794;
+let sum = num1 * num2;
+console.log("The sum of 1230941 times 1823794 equals:");
+//"This is a large sum"
+console.log(sum);
+let num3 = 637263;
+let num4 = 54;
+let sum2 = num3 / num4;
+console.log("The sum of 637263 devided by 54 equals:");
+console.log(sum2);
+
+// you will not see the comment because #1 its a comment and #2 you are not console logging it.(printing it out)
+
+// no you cannot see the results because you commented them out. Once your code is commented out it will not be read.
