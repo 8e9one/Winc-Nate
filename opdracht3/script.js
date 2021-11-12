d56f58fefc8f9cae5de3d6a494bf6abe33dc3e19
8e9one <33422357+8e9one@users.noreply.github.com>
2021-11-12 13:59:58 +0100

    Add files via upload

---
+++ opdracht3/script.js
@@ -0,0 +1,21 @@
+//PART 1
+const age = 17;
+if (age >= 18) {
+    console.log("You are old enough to enter.")
+} else {
+    console.log("You are not old enough to enter, sorry.")
+}
+//PART 2
+const isFemale = false;
+if (isFemale == true) {
+    console.log("Welcome and have a great ladies night!")
+} else {
+    console.log("Hey, you are not a woman.")
+}
+//PART 3
+const driverStatus = 'bob';
+if (driverStatus == 'bob') {
+    console.log("You are the bob, very responsible of you.(sucks to be you)")
+} else {
+    console.log("you are not the bob, have fun and try not to puke to much tomorrow.")
+}
