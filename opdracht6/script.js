8b23487b5707234dfe91af77da8f6e1069e28d7a
8e9one <33422357+8e9one@users.noreply.github.com>
2021-11-14 09:56:36 +0100

    Add files via upload

---
+++ opdracht6/script.js
@@ -0,0 +1,18 @@
+const makePizza = function () {
+    console.log("Step 1: Make pizza dough");
+    console.log("Step 2: Make sauce");
+    console.log("Step 3: Assemble and bake the pizza");
+}
+
+const makeSushi = function () {
+    console.log("Step 1: Pick up phone.");
+    console.log("Step 2: Open thuisbezorgd app.");
+    console.log("Step 3: Order Sushi.");
+    console.log("Step 4: Enjoy");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
